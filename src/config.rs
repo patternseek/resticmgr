8148c2128a1836560153fff97402d7a6f1dcc718
Tolan Blundell <tolan@patternseek.net>
2018-01-21 14:52:07 +0100

    Refactor config format. Add support for checking last snapshot in a repo.

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,12 @@ use serde_json;
 use failure::{Error};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub repos: HashMap<String, Repo>,
+    pub backupsets: Vec<BackupSet>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Repo {
     pub url: String,
     pub env: Option<HashMap<String, String>>,
@@ -13,11 +19,23 @@ pub struct Repo {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BackupSet {
-    pub repos: Vec<Repo>,
+    pub reponames: Vec<String>,
     pub dirs: Vec<String>,
 }
 
-pub fn load(config_file: String) -> Result<Vec<BackupSet>,Error> {
+impl BackupSet{
+    pub fn repos<'a>( &'a self, config: &'a Config ) -> Vec<&'a Repo>{
+        let mut repos: Vec<&'a Repo> = vec!();
+        for name in &self.reponames{
+            if let Some( ref mut repo ) = config.repos.get( name ){
+                repos.push(repo );
+            }
+        }
+        repos
+    }
+}
+
+pub fn load(config_file: String) -> Result<Config,Error> {
     let mut file = File::open(config_file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 extern crate failure;
+extern crate chrono;
 
 use failure::{Error, err_msg};
 use structopt::StructOpt;
 use std::process::Command;
 use scoped_threadpool::Pool;
 use std::collections::HashMap;
+use chrono::{DateTime, Utc};
+use std::process::exit;
 
 mod config;
-use config::BackupSet;
 use config::Repo;
-use std::process::exit;
+use config::Config;
 
 
 #[derive(StructOpt, Debug)]
@@ -26,6 +28,20 @@ struct Args {
     #[structopt(help = "Config file (JSON)", default_value = "./config.json")]
     config_file: String,
 }
+#[derive(Serialize, Deserialize, Debug)]
+struct SnapshotInfo{
+    time:  DateTime<Utc>,
+    parent: Option<String>,
+    tree: String,
+    paths: Vec<String>,
+    hostname: String,
+    username: String,
+    uid: isize,
+    gid: isize,
+    id: String,
+    short_id: String,
+
+}
 
 fn main() {
     let args = Args::from_args();
@@ -33,6 +49,7 @@ fn main() {
         Ok( conf ) => {
             match args.action.as_ref() {
                 "backup" => backup_all(&conf),
+                "verify" => check_last_snapshots_for_all( &conf ),
                 _ => eprintln!("Invalid command '{}'", args.action),
             }
         },
@@ -43,11 +60,64 @@ fn main() {
     }
 }
 
-fn backup_all(config: &[BackupSet]) {
+fn check_last_snapshots_for_all(config: &Config ){
     let mut thread_pool = Pool::new(4);
     let mut restic_results = HashMap::new();
-    for set in config {
-        for repo in &set.repos {
+    for (_,repo) in &config.repos {
+        thread_pool.scoped(|_scope| {
+            restic_results.insert(&repo.url, check_last_snapshot(repo));
+        });
+    }
+    for (repo, output) in restic_results {
+        println!("Repository '{}' results:", repo);
+        match output {
+            Ok(out) => println!("{}", out),
+            Err(out) => {
+                eprintln!("Failed:\n{}", out);
+                // FIXME Send notification email on errors
+            }
+        }
+    }
+}
+
+fn check_last_snapshot(repo: &Repo) -> Result<String, Error > {
+    println!("Checking snapshots for repo '{}'...", &repo.url);
+
+    let mut command = Command::new("restic");
+    command
+        .arg("snapshots")
+        .arg("--last")
+        .arg("--json");
+
+    setup_restic_standard_options( &repo, &mut command);
+
+    // Run
+    let output = command.output()?;
+    if output.status.success() {
+        let res = &*String::from_utf8_lossy(&output.stdout);
+        match serde_json::from_str(res) as Result<Vec<SnapshotInfo>, serde_json::Error> {
+            Ok(ref mut snapshots) => {
+                if let Some(ref snapshot) = snapshots.pop() {
+                    let duration_ago = Utc::now().signed_duration_since(snapshot.time);
+                    Ok(format!("Last backed up {} days, {} hours, {} mins ago. Paths: {}", duration_ago.num_days(), duration_ago.num_hours(), duration_ago.num_minutes(), snapshot.paths.join(", ")))
+                } else {
+                    Err( err_msg( "No snapshots found" ) )
+                }
+            },
+            Err( err ) => {
+                Err( err_msg( format!( "Couldn't parse response from restic: {}", err ) ) )
+            }
+        }
+    } else {
+        Err(err_msg(String::from_utf8_lossy(&output.stderr).to_string()))
+    }
+}
+
+fn backup_all(config: &Config) {
+    let mut thread_pool = Pool::new(4);
+    let mut restic_results = HashMap::new();
+    for set in &config.backupsets {
+        for repo in set.repos( config ) {
             thread_pool.scoped(|_scope| {
                 restic_results.insert(&repo.url, backup_to_single_repo(repo, &set.dirs));
             });
@@ -66,25 +136,15 @@ fn backup_all(config: &[BackupSet]) {
 }
 
 fn backup_to_single_repo(repo: &Repo, dirs: &[String]) -> Result<String, Error > {
-    println!("Starting backup to repo '{}'...", &repo.url);
+    println!("Verifying repo '{}'...", &repo.url);
 
     let mut command = Command::new("restic");
-    command.arg("backup")
-        .arg("-r")
-        .arg(&repo.url);
+    command
+        .arg("backup")
+        .arg("--json");
 
-    // Set env vars
-    if let Some(ref env) = repo.env {
-        command.envs(env);
-    }
 
-    // Set options
-    if let Some(ref options) = repo.options {
-        for option in options {
-            command.arg("-o");
-            command.arg(format!("{}={}", option.0, option.1));
-        }
-    }
+    setup_restic_standard_options( &repo, &mut command);
 
     // Set directories
     command.args(dirs.iter());
@@ -95,5 +155,23 @@ fn backup_to_single_repo(repo: &Repo, dirs: &[String]) -> Result<String, Error >
         Ok(String::from_utf8_lossy(&output.stdout).to_string())
     } else {
         Err(err_msg(String::from_utf8_lossy(&output.stderr).to_string()))
+    }
+}
+
+fn setup_restic_standard_options(repo: &Repo, command: &mut Command) {
+    // Set repo
+    command
+        .arg("-r")
+        .arg(&repo.url);
+    // Set env vars
+    if let Some(ref env) = repo.env {
+        command.envs(env);
+    }
+    // Set options
+    if let Some(ref options) = repo.options {
+        for option in options {
+            command.arg("-o");
+            command.arg(format!("{}={}", option.0, option.1));
+        }
     }
 }
